6f6e70188cdd9259bbb11194439bbc8cb5bfc2ec
Anca Gabriela Munteanu <ancag.munteanu@gmail.com>
2018-06-22 13:15:08 +0200

    Add files via upload
    updated

--- exercici workbench.sql
+++ exercici workbench.sql
@@ -3,33 +3,33 @@
 SELECT count(*) FROM USAirlineFlights.Flights;
 
 //	Retard promig de sortida i arribada segons l’aeroport origen.
-SELECT Origin, AVG(ArrDelay) AS ArrivalDelay FROM USAirlineFlights.Flights
-GROUP BY Origin;
-
-SELECT Origin, AVG(DepDelay) AS DepartureDelay FROM USAirlineFlights.Flights
+SELECT Origin, AVG(ArrDelay) AS ArrivalDelay, AVG(DepDelay) AS DepartureDelay FROM USAirlineFlights.Flights
 GROUP BY Origin;
 
 //Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen
-SELECT Origin, colMonth, AVG(ArrDelay) AS ArrivalDelay FROM USAirlineFlights.Flights
-GROUP BY Origin, colMonth;
+SELECT Origin, colYear, colMonth, AVG(ArrDelay) AS ArrivalDelay FROM USAirlineFlights.Flights
+GROUP BY Origin, colYear, colMonth;
 
 // Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen 
 (mateixa consulta que abans i amb el mateix ordre). Però a més, ara volen que 
 en comptes del codi de l’aeroport es mostri el nom de la ciutat.
 
-SELECT City, colMonth, AVG(ArrDelay) AS ArrivalDelay 
+SELECT City, colYear, colMonth, AVG(ArrDelay) AS ArrivalDelay 
 FROM Flights
 INNER JOIN USAirports ON Flights.Origin = USAirports.IATA
-GROUP BY Origin, colMonth;
+GROUP BY Origin, colYear, colMonth;
 
 //Les companyies amb més vols cancelats. A més, han d’estar ordenades de forma 
 que les companyies amb més cancelacions apareguin les primeres.
 
 
-SELECT DISTINCT Description, Cancelled
+SELECT DISTINCT Description, count(Cancelled)
 FROM Flights
 INNER JOIN Carriers ON Flights.UniqueCarrier = Carriers.CarrierCode
+WHERE Cancelled= 1
+GROUP BY Description
 ORDER BY Description ASC;
+
 
 
 //Companyies amb el seu promig només d’aquelles les quals els seus vols arriben al
@@ -44,6 +44,7 @@ HAVING AVG(ArrDelay) > 10;
 
 //L’identificador dels 10 avions que més kilòmetres han recorregut fent vols comercials:
 
-SELECT DISTINCT TailNum, Distance FROM Flights
-ORDER BY Distance DESC
+SELECT TailNum, SUM(Distance) FROM Flights
+GROUP BY TailNum
+ORDER BY SUM(Distance) DESC
 LIMIT 10;
